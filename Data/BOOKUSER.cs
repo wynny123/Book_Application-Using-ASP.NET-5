4dc3a460f6c9d22e1dd9aa302d72c5b2ce2ef129
Edwin <wynny123@gmail.com>
2021-09-25 08:51:00 +0100

    Logger configuration

---
+++ Data/BOOKUSER.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Book_Application.Data
+{
+    public class BOOKUSER
+    {   public int BookUser_ID { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+
+    }
+}
